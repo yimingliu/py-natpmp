ca78fc623f4061420f6f309796f4913bf3741aaa
homedirectory <47361170+homedirectory@users.noreply.github.com>
2023-02-06 11:43:03 +0200

    improved handling of mandatory arguments and structured the code

--- natpmp/natpmp_client.py
+++ natpmp/natpmp_client.py
@@ -8,20 +8,27 @@ try:
 except ImportError:
     import NATPMP
 
+USAGE="usage: natpmp-client.py [-u] [-l lifetime] [-g gateway_addr] public_port private_port"
+
 def main():
     if len(sys.argv) < 3:
-        print("usage: natpmp-client.py [-u] [-l lifetime] [-g gateway_addr] public_port private_port")
+        print(USAGE)
         sys.exit(-1)
 
     opts, args = getopt.getopt(sys.argv[1:], "ul:g:")    
+
+    if len(args) < 2:
+        print(USAGE)
+        sys.exit(-1)
+
     public_port = int(args[0])
     private_port = int(args[1])
+
     protocol = NATPMP.NATPMP_PROTOCOL_TCP
     lifetime = 3600
     gateway = None
     
-    for opt in opts:
-        name, val = opt
+    for name, val in opts:
         if name == "-u":
             protocol = NATPMP.NATPMP_PROTOCOL_UDP
         elif name == "-l":
@@ -31,7 +38,8 @@ def main():
 
     if not gateway:
         gateway = NATPMP.get_gateway_addr()
-    print NATPMP.map_port(protocol, public_port, private_port, lifetime, gateway_ip=gateway)
+
+    print(NATPMP.map_port(protocol, public_port, private_port, lifetime, gateway_ip=gateway))
 
 if __name__=="__main__":
     main()
